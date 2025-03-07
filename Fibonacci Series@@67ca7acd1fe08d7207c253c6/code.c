35f613ad138cb2576a42ebca134572bc21756744
RAMANPREET SAINI <ramanpreetsaini.18@gmail.com>
2025-03-07 14:29:46 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,22 +1,32 @@
 #include<stdio.h>
 
 void fibo(int n);
+
 int main(){
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     fibo(n);
 
     return 0;
 }
 
 void fibo(int n){
-    int a=0,b=1,c;
-    for(int i=1;i>n;i++){
-        printf("%d",a);
-        c=a+b;
-        a=b;
-        b=c;
-
+    int a = 0, b = 1, c;
+    if (n == 0) return;
+    if (n == 1) {
+        printf("%d", a);
+        return;
     }
 
+    for(int i = 1; i <= n; i++){
+        printf("%d", a);
+        if (i != n) {
+            printf(" ");
+        }
+        
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    printf("\n"); 
 }
