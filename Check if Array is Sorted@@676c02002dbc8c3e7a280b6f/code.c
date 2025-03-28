d6f709a99524af50eae4d3ea066dd96f30a800ba
RAMANPREET SAINI <ramanpreetsaini.18@gmail.com>
2025-03-28 11:23:28 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,30 +1,38 @@
-#include<stdio.h>
-void sorted(int *a,int n){
-    int temp,is_sorted;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if (a[j]>a[j+1]){
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            if(temp=is_sorted){
-                printf("Sorted");
+#include <stdio.h>
 
-            }
-            else{
-                printf("Not sorted");
-            }
+void sorted(int *a, int n) {
+    int temp;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
+    int is_sorted = 1;
+    for(int i = 0; i < n - 1; i++) {
+        if(a[i] > a[i + 1]) {
+            is_sorted = 0;
+            break;
+        }
+    }
+
+    if(is_sorted)
+        printf("Sorted\n");
+    else
+        printf("Not sorted\n");
 }
 
-int main(){
-    int a[20],n;
-    scanf("%d",&n);
-    for(int b=0;b<n;b++){
-        scanf("%d",a[b]);
+int main() {
+    int a[20], n;
+    scanf("%d", &n);
+    for(int b = 0; b < n; b++) {
+        scanf("%d", &a[b]);  
     }
-    return 0;
 
+    sorted(a, n);  
+
+    return 0;
 }
